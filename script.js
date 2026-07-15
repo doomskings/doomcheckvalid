e62e9ed34860d930adbba5f03d2d694c80484203
doomskings <kucingsetia98@gmail.com>
2026-07-15 13:55:48 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const tbody=document.getElementById("resultBody");
+
+tbody.innerHTML=`
+<tr>
+<td>google.com</td>
+<td>98</td>
+<td>100</td>
+<td>87</td>
+<td>1</td>
+<td>950000000</td>
+<td>✅</td>
+</tr>
+
+<tr>
+<td>facebook.com</td>
+<td>96</td>
+<td>99</td>
+<td>85</td>
+<td>2</td>
+<td>800000000</td>
+<td>✅</td>
+</tr>
+`;
